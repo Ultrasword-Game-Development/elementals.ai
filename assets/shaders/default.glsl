3a87f2466619066a674bbc81d4cd70b0a85637c8
Peter Zhang <34387360+Ultrasword@users.noreply.github.com>
2024-07-21 17:35:12 -0400

    removed blue filter

--- assets/shaders/default.glsl
+++ assets/shaders/default.glsl
@@ -5,8 +5,6 @@ in vec2 vert;
 in vec2 texcoord;
 out vec2 uvs;
 
-uniform float time;
-
 void main(){
     uvs = texcoord;
     gl_Position = vec4(vert, 0.0, 1.0);
@@ -22,6 +20,8 @@ in vec2 uvs;
 out vec4 color;
 
 void main(){
-    vec2 spos = vec2(uvs.x + sin(uvs.y * 10 + time) * 0.1, uvs.y);
-    color = vec4(texture(tex, spos).xy, texture(tex, spos).z * 1.5, 1.0);
+    time;
+    // vec2 spos = vec2(uvs.x + sin(uvs.y * 10 + time) * 0.1, uvs.y);
+    vec2 spos = uvs;
+    color = vec4(texture(tex, spos).xy, texture(tex, spos).z, 1.0);
 }
